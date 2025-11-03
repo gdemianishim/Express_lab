3db37c21d287688f868b4a24d62c8106463a1471
gdemianishim <georgedemian84@gmail.com>
2025-11-03 14:03:30 -0500

    Added users routs

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 
-const app = express(); //Calling this function sets up a server 
+const app = express()
+app.set("view engine", 'ejs');  //Calling this function sets up a server 
+
+
+app.get('/' , (req,res)=> {
+    console.log('Here');
+    res.render("index" , 
+        {user: "George!" ,
+}); 
+app.get('/users', (req, res => {
+    res.setEncoding("User List");
+    
+
+    app.get('/user/new', (req, res)=>{
+        res.send('New User form');
+    })
+}))
 
 app.listen(3030);
-
-app.get('/' , ()=> {
-    console.log('Here');
-    resizeBy.sen("Hello world");
-}); //this funtion will run when someone goes to the root folder.
